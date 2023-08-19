c6d1ced7b524194e49b96e27222048f7c6a82fef
Sam Johnson <s_johnso@yahoo.com>
2023-08-19 11:26:21 -0500

    added the server GET and POST routes for the feedback entries

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,3 +2,41 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+// GET all entries that have been placed
+router
+    .get('/', (req, res) => {
+        // Find all feedback entries and return them
+        pool
+            .query('SELECT * FROM "feedback";')
+            .then((result) => {
+                res.send(result.rows);
+            }).catch((error) => {
+                console.log('Error GET /feedback', error);
+                res.sendStatus(500);
+            });
+    })
+
+// POST a new entry
+router
+    .post('/', (req, res) => {
+        console.log('in router post')
+
+        const {
+            feeling,
+            understanding,
+            support,
+            comments,
+        } = req.body;
+        const sqlText = `INSERT INTO feedback ("feeling", "understanding", "support", "comments")
+                        VALUES ($1, $2, $3, $4);`
+
+        pool
+            .query(sqlText, [feeling, understanding, support, comments])
+            .then(response => {
+                res.sendStatus(201);
+            })
+            .catch(error => {
+                console.log('Error POST /feedback', error);
+                res.sendStatus(500);
+            })
+    });
