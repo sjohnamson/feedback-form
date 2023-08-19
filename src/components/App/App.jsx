ac60e43319fbc1a6468b0eec19b625c17fd916c3
Sam Johnson <s_johnso@yahoo.com>
2023-08-19 10:02:01 -0500

    setup basic structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 function App() {
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import { useDispatch, useSelector, } from 'react-redux';
+import axios from 'axios';
+import { useState } from 'react';
+
+import { useHistory } from 'react-router-dom'
+
+export default function Header() {
+    const history = useHistory();
+    const dispatch = useDispatch();
+}
